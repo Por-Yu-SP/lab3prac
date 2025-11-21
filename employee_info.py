7af3aae2982741954b433044b1e6d2f0d83aec14
PorYu <yu07.24@ichat.sp.edu.sg>
2025-11-21 22:52:24 +0800

    added calc avg

--- employee_info.py
+++ employee_info.py
@@ -20,12 +20,13 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 
 def calculate_average_salary():
     total = 0
-    average = 0
 
     #add your implementation to calculate here
+    for item in employee_data:
+        total+=item["salary"]
+    
 
-
-    return average
+    return total/len(employee_data)
 
 def get_employees_by_dept(department):
     result = []
